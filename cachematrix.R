9444fdfee8da5e9b7a92762f9d0f645d76a55fb7
Maz B Shaikh <mazahar.basha@gmail.com>
2015-09-26 13:37:06 -0500

    Author: Mazahar Basha Shaikh Date: Sep 26, 2015

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script stores the argument matrix and calculates its inverse.
+## If the matrix is already inversed, this script will get the inverse matrix
+## from the cache instead of calculating again.
+## Condition: Pass only inversible matrix
+## Author: Mazahar Basha Shaikh Date: 09.26.2015
 
-## Write a short comment describing this function
+## Get the matrix and store the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x)
+{
 
+  matr <- NULL          # initialize 
+  
+  set <- function(y)    # a valid matrix needs to be passed
+  {
+    x <<- y
+    matr <<- NULL
+  }
+  
+  getMat <- function()     # print the argument matrix
+  {
+    x
+  }
+  
+  storeInv <- function(imat) # Store inverse matrix
+  {
+    
+    matr <<- imat
+    # matr <<- solve(x)
+  }
+  
+  getInv <- function()  # print the inverse matrix
+  {
+    matr
+  }
+  
+  list(set = set, getMat = getMat, storeInv = storeInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function reads the above function and calculates inverse
+
+cacheSolve <- function(b)
+{
+  
+  m <- b$getInv()
+  
+  if (!is.null(m))
+  {
+    message("getting cached data")
+    print(m)
+    
+  }
+  
+  mat <- b$getMat()
+  imat <- solve(mat)
+  b$storeInv(imat)
+  
+  imat
+  
 }
